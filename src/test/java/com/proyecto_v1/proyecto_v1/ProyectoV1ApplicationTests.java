c528a4d59f69c679819e5dd914baa67617b2d35c
victor javier vivas <victorjaviervivas@gmail.com>
2024-04-07 09:12:55 -0400

    Commit inicial: Creación del proyecto

---
+++ src/test/java/com/proyecto_v1/proyecto_v1/ProyectoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_v1.proyecto_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
